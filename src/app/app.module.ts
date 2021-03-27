96505fe4b762e41c9589469cb2f596b5a7a6826c
baraskarharshal <baraskarharshal@gmail.com>
2021-03-28 00:31:43 +0530

    5 files modified, 10 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { AppRoutingModule } from "./app.routing.module";
+import { Demo1Component } from "./features/demo1/demo1.component";
+import { Demo2Component } from "./features/demo2/demo2.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    SidebarComponent,
+    Demo1Component,
+    Demo2Component
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { Demo1Component } from "./features/demo1/demo1.component";
+import { Demo2Component } from "./features/demo2/demo2.component";
+
+const routes: Routes = [
+  {
+    path: "demo1",
+    component: Demo1Component
+  },
+  {
+    path: "demo2",
+    component: Demo2Component
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
