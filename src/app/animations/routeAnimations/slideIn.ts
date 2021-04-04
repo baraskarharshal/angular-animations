aa73f8ba4194cecf30766eb2493a5e237b7c8e1d
baraskarharshal <baraskarharshal@gmail.com>
2021-04-04 23:08:31 +0530

    2 files modified

--- src/app/animations/routeAnimations/slideIn.ts
+++ src/app/animations/routeAnimations/slideIn.ts
@@ -9,8 +9,12 @@ import {
 } from "@angular/animations";
 
 export const slideInAnimation = trigger("routeAnimations", [
+  // transition defined for two way
   transition("HomePage <=> AboutPage", [
+    // set relative position of current element
     style({ position: "relative" }),
+    // :enter and :leave represents HomePage or AboutPage
+    // query finds these child html elements inside current html element and assign given styles to them. In this case, both demo1 and demo2 components.
     query(":enter, :leave", [
       style({
         position: "absolute",
@@ -19,10 +23,14 @@ export const slideInAnimation = trigger("routeAnimations", [
         width: "100%"
       })
     ]),
+    // query finds component which is entering and sets it left position to -100.
     query(":enter", [style({ left: "-100%" })]),
     query(":leave", animateChild()),
+    // group runs all queries in parallel
     group([
+      // leaving component is going out
       query(":leave", [animate("500ms ease-out", style({ left: "100%" }))]),
+      // entering component is coming in
       query(":enter", [animate("500ms ease-out", style({ left: "0%" }))])
     ]),
     query(":enter", animateChild())
